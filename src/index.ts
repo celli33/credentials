35a13196c4af029d9b04f848be0397d4ce5830ce
Fernando IL <luffynando@gmail.com>
2021-06-21 14:18:09 -0500

    feat: better document package.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-/** Hello world */
+import { Certificate } from './Certificate';
+import { Credential } from './Credential';
+import { AlgoSign } from './internal/AlgorithmSignatureEnum';
+import { SatTypeEnum } from './internal/SatTypeEnum';
+
+export { Certificate, Credential, AlgoSign, SatTypeEnum };
+
+Object.assign(module.exports, Certificate);
+Object.assign(module.exports, Credential);
+Object.assign(module.exports, AlgoSign);
+Object.assign(module.exports, SatTypeEnum);
